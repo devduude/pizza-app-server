0244436f343fb988f8a160728d5117e6a1ff6818
Eugene Rodionov <devduude@gmail.com>
2020-06-27 20:18:20 +0300

    Feature(index): setup hapi server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-function asd (params) {
-  console.log('wow');
+import Hapi from '@hapi/hapi';
+
+import routes from './routes.js';
+
+const server = Hapi.server({
+  port: process.env.PORT || 3030,
+  host: process.env.HOST || 'localhost',
+  routes: { cors: { origin: [ '*' ] } },
+});
+
+async function init () {
+  await server.route(routes);
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 }
 
-export default asd;
+/**
+ * Process event,
+ * Triggered by Ctrl+C
+ * Stops the server
+ */
+process.on('SIGINT', () => {
+  console.log('Stopping the server');
+
+  server.stop({ timeout: 10 * 1000 })
+    .then(() => process.exit(0))
+    .catch(() => process.exit(1));
+});
+
+/**
+ * Process event,
+ * Triggered by unhandled rejection
+ * Stops the server
+ */
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  console.log('unhandledRejection', err, 'error');
+
+  server.stop({ timeout: 10 * 1000 })
+    .finally(() => process.exit(1));
+});
+
+
+export default init;
